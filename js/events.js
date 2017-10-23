94b62048f9e7a4bea8541336e7bd4102f8ae3cd0
Oleg Bondarchuk <bozzard@gmail.com>
2017-10-23 14:12:41 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,11 @@
 //define functions here
 
+$(document).on('keypress', getIt() {
+  if (key.which == 13){
+    alert('enter was pressed');
+  }
+});
+
 $(document).ready(function(){
 
 // call functions here
