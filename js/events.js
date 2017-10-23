7f9d54172c82ddc14af790cee77d8aa3b61ee448
Oleg Bondarchuk <bozzard@gmail.com>
2017-10-23 14:41:17 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,12 +3,12 @@ function getIt() {
 $('p').on('click', function() {
     alert("Hey!");
 });
-
+};
 function frameIt(){
 $('img').on('load', function() {
     $('img').addClass("tasty");
 });
-}
+};
 $(document).ready(function(){
 
 // call functions here
